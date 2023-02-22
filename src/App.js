44292aa0149b128d32e14c7ed637c0bc99aeb969
Aksha <akshaali01234@gmail.com>
2023-02-22 20:48:09 +0530

    static data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export const ContextInstance = createContext();
 function App() {
   const [name, setName] = useState("Aksha");
   return (
-    <ContextInstance.Provider value={{ name: name, setName: setName }}>
+    <ContextInstance.Provider value={{ name: name, setName: setName, static: "Aggdjg" }}>
       <div className="App">
         <ComponentA name={name} />
       </div>
